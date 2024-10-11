7a8978346468a0925a424a2e12539d0db98fbf12
Sandy030604 <santhosh.krishnan@ascendion.com>
2024-10-11 12:09:46 +0530

    Fist Commit

---
+++ src/test/java/package1/TC001_Stringsk.java
@@ -0,0 +1,23 @@
+package package1;
+
+public class TC001_Stringsk {
+	public static void main(String args[])
+	{
+		String str1="Santhosh";
+		String str2=str1;
+		System.out.println(str2);
+		str2="Krishnan";
+		System.out.println(str2);
+		
+		String str3="This is a beautiful day";
+		System.out.println("core "+str3);
+		
+		System.out.println("Length of str3: "+str3.length());
+		
+		System.out.println("Index of a: "+str3.indexOf("a"));
+		
+		System.out.println("Substring: "+str3.substring(10,20));
+		
+	}
+
+}
---
+++ src/test/java/package1/TC009_Wrapper_Class.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class TC009_Wrapper_Class {
+	public static void main(String args[])
+	{
+		String str="1234";
+		int num=Integer.parseInt(str);
+		System.out.println(str+200);
+		System.out.println(num+200);
+		double b=4.8;
+		int a=(int)b;
+	}
+
+}
